0b691b7fbbd48b3b8252d64f22da434e5d3913e3
lokpavan03 <80965262+lokpavan03@users.noreply.github.com>
2021-03-19 23:57:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,16 @@ provider "azurerm" {
 #   tenant_id       = ${env.tenant_id}
 }
 
+#Create a Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "WEgroup"
   location = "West Europe"
+}
+
+#Create a Virtual Network with resource group
+resource "azurerm_virtual_network" "vnet" {
+  name                = "Vnet-tf"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
 }
