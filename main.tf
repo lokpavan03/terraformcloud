5e596dc29f93896b16289f9c320d311625da6995
lokpavan03 <80965262+lokpavan03@users.noreply.github.com>
2021-03-20 12:51:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,9 +41,9 @@ resource "azurerm_resource_group" "rg" {
 }
 
 #Create a Virtual Network with resource group
-resource "azurerm_virtual_network" "vnet" {
-  name                = "Vnet-tf"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  address_space       = ["10.0.0.0/16"]
-}
+# resource "azurerm_virtual_network" "vnet" {
+#   name                = "Vnet-tf"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   address_space       = ["10.0.0.0/16"]
+# }
